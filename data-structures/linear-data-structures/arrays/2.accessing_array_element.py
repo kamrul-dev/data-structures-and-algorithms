8652e4119ea054e59a2dd39eba397a3dd64702d8
Kamrul Hasan (ICT) <kamrul.babu@liz.com>
2025-01-30 11:38:50 +0600

    Add accessing array elements and modify array elements

---
+++ data-structures/linear-data-structures/arrays/2.accessing_array_element.py
@@ -0,0 +1,6 @@
+arr = [10, 20, 30, 40, 50]
+# First element (index 0)
+print(arr[0])  # Output: 10
+
+# Third element (index 2)
+print(arr[2])  # Output: 30
---
+++ data-structures/linear-data-structures/arrays/3.modify_array_elements.py
@@ -0,0 +1,8 @@
+arr = [1, 2, 3, 4]
+arr[2] = 99  # Change the third element
+print(arr)  # Output: [1, 2, 99, 4]
+
+
+arr = [1, 3, 5, 6,7 ,8, 7]
+arr[3] = 5
+print(arr) # Output: [1, 3, 5, 5, 7, 8, 7]
