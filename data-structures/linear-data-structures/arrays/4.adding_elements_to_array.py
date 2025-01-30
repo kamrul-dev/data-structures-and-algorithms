db5006715e65ff654ad8e2551aaf621741bb216c
Kamrul Hasan (ICT) <kamrul.babu@liz.com>
2025-01-30 12:13:47 +0600

    Add adding elemensts to and array

---
+++ data-structures/linear-data-structures/arrays/4.adding_elements_to_array.py
@@ -0,0 +1,12 @@
+arr = [1, 2, 3]
+arr.append(4)  # Adds 4 to the end of the array
+print(arr)  # Output: [1, 2, 3, 4]
+
+arr.insert(1, 5)  # Inserts 5 at index 1
+print(arr)  # Output: [1, 5, 2, 3, 4]
+
+
+
+myarr = [1, 2, 3, 4, 5]
+myarr.append(7)
+print(myarr) # Output: [1, 2, 3, 4, 5, 7]
