4494ce095ec8a0940859eec4d271e7a6d7e79108
Kamrul Hasan (ICT) <kamrul.babu@liz.com>
2025-01-30 12:16:49 +0600

    Add remove elements from array elemenst

---
+++ data-structures/linear-data-structures/arrays/5.remove_elements_from_arry.py
@@ -0,0 +1,21 @@
+arr = [10, 20, 30, 40]
+
+arr.remove(20)  # Removes the element with value 20
+print(arr)  # Output: [10, 30, 40]
+
+arr.pop(1)  # Removes the element at index 1 (30)
+print(arr)  # Output: [10, 40]
+
+
+myarr = [1, 2, 3, 4, 5]
+myarr.remove(1)
+print(myarr) # Output: [2, 3, 4, 5]
+
+
+
+myarr = [1, 2, 3, 4, 5]
+myarr.pop(2)
+print(myarr) # Output: [1, 2, 4, 5]
+
+
+
