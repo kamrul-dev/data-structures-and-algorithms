2cab14f0fe777348ebb26c3323611df28519cdf5
Kamrul Hasan (ICT) <kamrul.babu@liz.com>
2025-01-30 11:23:20 +0600

    Create Array, Empty Array, Array list

--- data-structures/linear-data-structures/arrays/initial.py
+++ data-structures/linear-data-structures/arrays/initial.py
@@ -1,5 +1,15 @@
+# An empty array
+arr = []
+print(arr)
+
+# An array with values
 arr = [1, 2, 3, 4, 5]
-print(arr) 
+print(arr)
+
+# An array with different types of data
+arr = [1, "hello", 3.14, True]
+
+print(arr)
 
 
 
